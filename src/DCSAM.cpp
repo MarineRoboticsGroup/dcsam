7eaa803a9756ade9467ed7f84aa7e1024dee3049
Kevin <kdoherty@mit.edu>
2021-11-29 18:56:47 -0500

    MacOS changes and a simple elimination test.

--- src/DCSAM.cpp
+++ src/DCSAM.cpp
@@ -164,8 +164,8 @@ DCValues DCSAM::calculateEstimate() const {
 DCMarginals DCSAM::getMarginals(const gtsam::NonlinearFactorGraph &graph,
                                 const gtsam::Values &continuousEst,
                                 const gtsam::DiscreteFactorGraph &dfg) {
-  return DCMarginals{.continuous = gtsam::Marginals(graph, continuousEst),
-                     .discrete = gtsam::DiscreteMarginals(dfg)};
+  return DCMarginals(gtsam::Marginals(graph, continuousEst),
+                     gtsam::DiscreteMarginals(dfg));
 }
 
 }  // namespace dcsam
